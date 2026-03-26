da10dadd0fec4dd9fc3f5f6dd821e26fb2522709
Orkhan Dzhafarli <orhan.jafarliev@gmail.com>
2026-03-26 20:57:54 +0300

    feat: add max id length

--- js/piano.js
+++ js/piano.js
@@ -6,14 +6,18 @@ pianoKeys.forEach((key) => {
 		console.log(key);
 		e.stopPropagation();
 		const note = key.getAttribute("data-note");
-		playNote(note);
+		
+		if (pianoDisplay.textContent.length < 4) {
+			pianoDisplay.textContent = pianoDisplay.textContent.trim() + note;
+			playNote(note);
+		} else {
+			// TODO: find another sound
+		}
 	});
 });
 
 function playNote(note) {
 	// add note to display
-	pianoDisplay.textContent = pianoDisplay.textContent.trim() + note;
-	
 	// TODO: change to valid audio file path
 	// const audio = new Audio(`assets/sounds/${note}.mp3`);
 	const audio = new Audio(`assets/sounds/1.mp3`);
